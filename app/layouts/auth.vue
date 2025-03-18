0c97a4a3214e7a387d7520678548755cbc9223df
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-18 17:44:44 +0100

    add auth layout

---
+++ app/layouts/auth.vue
@@ -0,0 +1,9 @@
+<template>
+  <the-header/>
+  <main class="n-stack-horizontal ">
+    <provet-stack class="n-stack n-margin-auto n-container-xxs n-padding-b-xl n-padding-l" direction="vertical"
+                  align-items="stretch" justify-content="center" gap="m">
+      <slot/>
+    </provet-stack>
+  </main>
+</template>
