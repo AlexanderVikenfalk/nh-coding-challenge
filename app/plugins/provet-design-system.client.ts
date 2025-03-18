0adf3b328adba02deb133282d97f348797a2f68b
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-18 17:42:02 +0100

    add provet styling as plugin

--- app/plugins/provet-design-system.client.ts
+++ app/plugins/provet-design-system.client.ts
@@ -1,4 +1,14 @@
 import "@provetcloud/css";
-import "@provetcloud/web-components"
+import '@provetcloud/web-components/lib/Button'
+import '@provetcloud/web-components/lib/Icon'
+import '@provetcloud/web-components/lib/Input'
+import '@provetcloud/web-components/lib/Card'
+import '@provetcloud/web-components/lib/Header'
+import '@provetcloud/web-components/lib/Stack'
+import '@provetcloud/web-components/lib/Tooltip'
+import '@provetcloud/web-components/lib/Checkbox'
+import '@provetcloud/web-components/lib/Layout'
+import '@provetcloud/web-components/lib/Spinner'
+import '@provetcloud/web-components/lib/Banner'
 
 export default defineNuxtPlugin(() => ({}));
