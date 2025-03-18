638a88d427950a49556c3f89e283586edafaf7cc
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-18 17:46:10 +0100

    add succcess page

---
+++ app/pages/registration/success.vue
@@ -0,0 +1,30 @@
+<template>
+  <provet-card class="n-align-center">
+    <provet-icon size="xl" name="interface-checked-circle" color="green"/>
+    <h2>Registration Successful</h2>
+    <h4>Welcome {{ user.email }}</h4>
+    <span v-if="user.receiveNewsletter">Thank you for subscribing to our newsletter!</span>
+  </provet-card>
+</template>
+
+<script setup>
+import {useUserStore} from '@/stores/user';
+
+const userStore = useUserStore();
+const {user} = storeToRefs(userStore);
+
+onMounted(() => {
+  if (user.isSuccess === false) {
+    user.isSuccess = true;
+  }
+})
+
+useHead({
+  title: 'Registration Successful'
+})
+definePageMeta({
+  layout: "auth",
+  middleware: "auth-success"
+})
+
+</script>
