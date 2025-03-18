280ffcf1fdb5b86ca77b60b74c4f95bcf7c1fd80
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-18 17:44:01 +0100

    set nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4
-  },
-  ssr: false,
-  spaLoadingTemplate: true,
-  vite: {
-    vue: {
-     template: {
-       compilerOptions: {
-         isCustomElement: (tag) => tag.includes('-')
-       }
-     }
-    }
-  }
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@pinia/nuxt'],
+    future: {
+        compatibilityVersion: 4
+    },
+    typescript: {
+        typeCheck: true
+    },
+    ssr: false,
+    spaLoadingTemplate: true,
+    vite: {
+        vue: {
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag.includes('-')
+                }
+            }
+        },
+    },
 })
