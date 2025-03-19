949ed6910f2b018600139b5823d9beda6745aa97
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-19 20:47:05 +0100

    add user schema

---
+++ utils/userSchema.ts
@@ -0,0 +1,18 @@
+import {z} from 'zod';
+
+export const userSchema = z.object({
+    email: z.string().email({message: "Please enter a valid E-mail address."}),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+    receiveNewsletter: z.boolean().optional()
+}).superRefine(({confirmPassword, password}, ctx) => {
+    if (confirmPassword !== password) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'The passwords did not match',
+            path: ['confirmPassword']
+        })
+    }
+})
+
+export type UserData = z.infer<typeof userSchema>
