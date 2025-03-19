fa11aa039f565adc9437eafa7bb54a7b4cb9ad5f
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-19 20:43:57 +0100

    add nuxt config

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    css: ['@provetcloud/css'],
+    modules: ['@pinia/nuxt', '@nuxt/test-utils/module'],
+    ssr: false,
+    spaLoadingTemplate: true,
+    vite: {
+        vue: {
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag.includes('-')
+                }
+            }
+        },
+    },
+});
