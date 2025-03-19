caa2fed2896093533c42cb24ae0203e8f7fd592d
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-03-19 20:47:28 +0100

    add success page

---
+++ pages/registration/success.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="n-align-center">
+    <provet-icon size="xxl" name="interface-checked-circle" color="green"/>
+    <section class="n-padding-m">
+      <h1 class="n-typescale-xxl">Registration Successful</h1>
+      <h4>Welcome, {{ user?.email }}</h4>
+      <p v-if="user?.receiveNewsletter" class="n-color-text-weaker">Thank you for subscribing to our newsletter!</p>
+    </section>
+    <NuxtLink to="/">
+      <provet-button @click="resetCurrentUser" variant="primary">Home</provet-button>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const userStore = useUserStore();
+const user = computed(() => userStore.user);
+
+useHead({
+  title: 'Registration Successful'
+})
+definePageMeta({
+  middleware: "redirect-to-register"
+})
+
+function resetCurrentUser() {
+  userStore.reset();
+}
+
+</script>
